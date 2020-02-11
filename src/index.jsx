5ae340f2d737c633abc534ae76def041ca6fb7e2
Nicolas Oga <2112.oga@gmail.com>
2020-02-11 16:42:11 -0300

    initial commit

---
+++ src/index.jsx
@@ -0,0 +1,13 @@
+import 'regenerator-runtime/runtime'
+import 'normalize.css'
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = _props => (
+  <div>
+    Hello World
+  </div>
+)
+
+ReactDOM.render(<App />, document.getElementById('root'))
